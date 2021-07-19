d29a1608a5d6b9d48c6ee442a7edc2a46c4ace9b
DamianoD88 <dispe88@live.it>
2021-07-19 12:15:44 +0200

    add second

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+//   props: {
+//     msg: String
+//   }
+}
+</script>
+
+<style lang="scss">
+
+</style>
